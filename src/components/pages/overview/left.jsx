6f31c43b9bf21b3cb856ee37a77df7e14eaa3326
Ngocvovn <duyngocvo@gmail.com>
2017-11-02 06:38:19 +0200

    fix left component

--- src/components/pages/overview/left.jsx
+++ src/components/pages/overview/left.jsx
@@ -25,7 +25,6 @@ class OverviewLeft extends React.Component {
     };
     this.count(root, statistic, map);
     statistic.allSensors += map.size;
-    console.log(statistic);
     return statistic;
   }
 
@@ -62,14 +61,14 @@ class OverviewLeft extends React.Component {
 
   render() {
     var overviewData = this.props.overview;
-    this.countTreeStatistic(this.props.currentNode, this.props.currentSensor);
+    var stas = this.countTreeStatistic(this.props.currentNode, this.props.currentSensor);
     return (
       <div style={{marginTop:'20px'}} className="overview-block">
         <div className="container-fluid">
           <div className="row">
             <div className="col-sm-4 col-xs-12">
               <div className="the-card clearfix">
-                <h1 className="card-location">Main Office</h1>
+                <h1 className="card-location">{this.props.currentNode.info.name}</h1>
                 <div className="card-buttons clearfix">
                   <div className="card-inner-location pull-left"><span>Meeting Rooms </span><i className="fa fa-chevron-down" aria-hidden="true"></i>
                     <div className="dropdown card-area-dropdown">
@@ -87,13 +86,13 @@ class OverviewLeft extends React.Component {
                 <div className="card-stats-numeric clearfix">
                   <div className="total-rooms-container text-center pull-left">
                     <div className="total-rooms-title"><span>Total Rooms</span></div>
-                    <div className="total-rooms-number"><span>42</span></div>
-                    <div className="total-rooms-inuse"><span id="rooms-inuse">12</span><span> in use   </span></div>
+                    <div className="total-rooms-number"><span>{stas.allRooms}</span></div>
+                    <div className="total-rooms-inuse"><span id="rooms-inuse">{stas.roomsInUse}</span><span> in use   </span></div>
                   </div>
                   <div className="total-desks-container text-center pull-right">
                     <div className="total-desks-title"><span>Total Desks</span></div>
-                    <div className="total-desks-number"><span>42</span></div>
-                    <div className="total-desks-inuse"><span id="desks-inuse">12</span><span> in use   </span></div>
+                    <div className="total-desks-number"><span>{stas.allSensors}</span></div>
+                    <div className="total-desks-inuse"><span id="desks-inuse">{stas.desksInUse}</span><span> in use   </span></div>
                   </div>
                 </div>
                 <div className="card-bottom-menu">
--- src/config/development.js
+++ src/config/development.js
@@ -2,6 +2,6 @@ import base from './base';
 
 export default Object.assign(base, {
   api: {
-    root: 'http://ec2-54-229-187-138.eu-west-1.compute.amazonaws.com:8081/api'
+    root: 'http://ec2-52-213-47-172.eu-west-1.compute.amazonaws.com:8081/api'
   }
 });

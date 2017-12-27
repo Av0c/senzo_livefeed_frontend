481767134521f490b5ef77d787951febfab0d1f4
Unknown <duyngocvo@gmail.com>
2017-12-27 14:41:46 +0200

    fix location dropdown

--- src/containers/toolbar.jsx
+++ src/containers/toolbar.jsx
@@ -7,6 +7,19 @@ import { fetchLiveData } from 'actions/node';
 
 export class Toolbar extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = { show: false };
+  }
+
+  showChildren() {
+    this.setState({ show: true });
+  }
+
+  closeChildren() {
+    this.setState({ show: false });
+  }
+
   componentDidMount() {
     this.props.dispatch(fetchCustomerOverview(this.props.user.rootnodeid));
   }
@@ -16,12 +29,15 @@ export class Toolbar extends React.Component {
       <div style={{ width: '100%', backgroundColor: 'white', paddingBottom: '15px' }} className="container-fluid">
         <div className="row">
           <div className="col-xs-4" style={{ zIndex: '1007' }}>
-            <div className="location-block clearfix">
+            <div onMouseOver={this.showChildren.bind(this)} className="location-block clearfix">
               <div className="location-icon pull-left"><img src="src/assets/images/location-icon.svg" alt="Location" /></div>
               <div className="location-name pull-left"><span>{this.props.companyName}</span></div>
-              <div style={{ marginTop: '13px' }} className="location-dropdown-root">
-                <Tree tree={this.props.tree} statistic={this.props.statistic} />
-              </div>
+              {this.state.show && <div>
+                <div style={{ backgroundColor: 'transparent' }} className={"modal-overlay" + (this.state.show ? "" : " closed")} onClick={this.closeChildren.bind(this)}></div>
+                <div style={{ marginTop: '13px', zIndex: 1000 }} className={"location-dropdown-root"}>
+                  <Tree tree={this.props.tree} statistic={(node) => { this.props.statistic(node); this.closeChildren(); }} />
+                </div>
+              </div>}
             </div>
           </div>
           <div className="col-xs-4">
@@ -34,7 +50,7 @@ export class Toolbar extends React.Component {
               <div className="settings-dropdown-root">
                 <ul>
                   <li><Link to="/settings/ownaccount">Own Account</Link></li>
-                  <li><Link to="/user">{ (this.props.user.role == "ADMIN") ? "User Administration" : "Contact" }</Link></li>
+                  <li><Link to="/user">{(this.props.user.role == "ADMIN") ? "User Administration" : "Contact"}</Link></li>
                   {
                     (this.props.user.role == "ADMIN") ? [
                       <li key="0"><Link to="locations">Locations Settings</Link></li>,
@@ -42,7 +58,7 @@ export class Toolbar extends React.Component {
                       <li key="2"><Link to="/api">Live Feed/SenzoAPI</Link></li>,
                       <li key="3"><Link to="/settings/default">Default Settings</Link></li>
                     ]
-                    : null
+                      : null
                   }
                   <li><Link to="/help">Help !</Link></li>
                   <li><a href="#" onClick={this.props.actions.logout}>Log Out</a></li>
--- src/containers/tree.jsx
+++ src/containers/tree.jsx
@@ -6,27 +6,33 @@ export default class Tree extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state= { show: true};
-    }  
+        this.state = { show: true};
+    }
+
+    openChildren() {
+        if (!this.state.show) {
+            this.setState({ show: true });
+        }
+    }
 
     render() {
         var childNodes;
         var style = {};
-        var self = this;    
-        if (this.props.tree.children != null && this.props.tree.type!='meeting_room' && this.props.tree.type!='open_area') {
-            if(this.props.tree.children.length >7){
-                style ={
+        var self = this;
+        if (this.props.tree.children != null && this.props.tree.type != 'meeting_room' && this.props.tree.type != 'open_area') {
+            if (this.props.tree.children.length > 7) {
+                style = {
                     overflowY: 'auto',
                     overflowX: 'hidden',
                     maxHeight: '200px'
                 };
             }
-            childNodes = this.props.tree.children.map(function(node, index) {
+            childNodes = this.props.tree.children.map(function (node, index) {
                 return <Node node={node} key={index} statistic={self.props.statistic.bind(self)}><Tree statistic={self.props.statistic.bind(self)} tree={node} /></Node>
             });
         }
         return (
-            <ul style={style}> 
+            <ul onMouseOver={this.openChildren.bind(this)} style={style}>
                 {this.state.show && childNodes}
             </ul>
         );

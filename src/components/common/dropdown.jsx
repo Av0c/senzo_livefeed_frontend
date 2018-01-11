7a1ef322ad184db2ae54d35a2f8c04025eec7ff9
Av0c <mindstorm1998@gmail.com>
2018-01-11 08:30:30 +0200

    Reposition cards on overview page, remove x-scroll (temp fix)

--- src/components/common/dropdown.jsx
+++ src/components/common/dropdown.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import DropdownItems from 'components/common/dropdownitems';
 
 export default class DropDown extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={opened:false};
-  }
+    constructor(props) {
+        super(props);
+        this.state={opened:false};
+    }
 
-  handleClick(){
-    this.setState({opened: !this.state.opened });
-  }
+    handleClick() {
+        this.setState({opened: !this.state.opened });
+    }
 
-  close(){
-    this.setState({opened: false });
-  }
+    close() {
+        this.setState({opened: false });
+    }
 
-  render () {
-    return (
-      <div className={`dropdown card-area-dropdown ${this.state.opened ? "dropdown open" : "dropdown"} ${this.props.customClass}`}>
-        <button className="dropdown-toggle" onClick={this.handleClick.bind(this)}>
-          {this.props.header}<i className={this.state.opened ? "icon arrow-up" : "icon arrow-down"}></i>
-        </button>
-        <div className="dropdown-items">
-          <DropdownItems close={this.props.toggleable ? this.close.bind(this) : () => {}}>
-            {this.props.children}
-          </DropdownItems>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className={`card-area-dropdown ${this.state.opened ? "dropdown open" : "dropdown"} ${this.props.customClass}`}>
+                <button className="dropdown-toggle" onClick={this.handleClick.bind(this)}>
+                    {this.props.header}<i className={this.state.opened ? "icon arrow-down" : "icon arrow-up"}></i>
+                </button>
+                <div className="dropdown-items">
+                    <DropdownItems close={this.props.toggleable ? this.close.bind(this) : () => {}}>
+                        {this.props.children}
+                    </DropdownItems>
+                </div>
+            </div>
+        )
+    }
 }

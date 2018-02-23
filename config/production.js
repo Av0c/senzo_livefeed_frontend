d846d48438ce916b97a93fdb1d2570480c5f935a
pqhuy98 <huypham060398@gmail.com>
2018-02-23 11:29:01 +0200

    obfuscate code in production

--- config/production.js
+++ config/production.js
@@ -1,8 +1,35 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var base = require('./base');
 var _ = require('lodash');
 
 var productionConfig = {
-
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    function () {
+      this.plugin("done", function (stats) {
+        if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1) {
+          process.exit(1); // or throw new Error('webpack build failed.');
+        }
+      });
+    },
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV) || JSON.stringify("staging")
+      }
+    }),
+    new HtmlWebpackPlugin(
+      {
+        template: 'src/index.html'
+      }
+    ),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'src/assets' }
+    ])
+  ]
 };
 
 module.exports = _.merge(base, productionConfig);

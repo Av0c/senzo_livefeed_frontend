22c7f397adf7182fb0d65925404f7f24c124e58d
pqhuy98 <huypham060398@gmail.com>
2018-05-29 18:07:15 +0300

    now using cloud server instead of local server.

--- src/config/development.js
+++ src/config/development.js
@@ -2,8 +2,8 @@ import base from './base';
 
 export default Object.assign(base, {
   api: {
-    // root: 'https://backend.senzodata.com/api'
+    root: 'https://backend.senzodata.com/api'
     // root: 'http://localhost/api'
-    root: 'http://91.158.228.239/api'
+    // root: 'http://91.158.228.239/api'
   }
 });

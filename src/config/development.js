ab586daf6d73be99dedf4a109f12d48394202dc7
pqhuy98 <huypham060398@gmail.com>
2018-10-22 22:36:07 +0300

    change url to backend-dev.senzodata.com

--- src/config/development.js
+++ src/config/development.js
@@ -2,8 +2,7 @@ import base from './base';
 
 export default Object.assign(base, {
   api: {
-    root: 'http://91.158.228.94/livefeedapi'
-    // root: 'https://backend.senzodata.com/livefeedapi'
+    root: 'https://backend-dev.senzodata.com/livefeedapi'
     // root: 'http://localhost/livefeedapi'
   }
 });
